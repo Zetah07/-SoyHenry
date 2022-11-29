954cc9dc4f14ff0c6db1431bfac209d02042365a
Johan Sebastian Castro Lopez <sebastiancastro0792@gmail.com>
2022-11-28 20:11:51 -0500

    actualizacion 06 react

--- FT-M2-master/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ FT-M2-master/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Johan Sebastian Castro Lopez";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
-}
+  return(
+    <><div>
+      <h1>mira mamá ya se programar</h1>
+      <h3>{studentName}</h3>
+      <ul>{techSkills.map((value, index)=> <li key={index}>{value}</li>)}</ul>
+      <Botones alerts={alerts}/>
+    </div>
+    </>
+
+  )
+};
 
 // Esto lo exportamos para los tests
 export { studentName, techSkills, alerts };
