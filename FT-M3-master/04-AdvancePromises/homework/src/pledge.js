de3f44a2e116cba8cd656f76f2c690314f47faf2
Johan Sebastian Castro Lopez <sebastiancastro0792@gmail.com>
2022-12-22 22:00:28 -0500

    point 2 and 3 complete of advance promises

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -10,22 +10,46 @@ function $Promise(executor){
 
         this._state = 'pending'; 
         this._value = undefined;
+        this._handlerGroups = [];
         
         executor(this._internalResolve.bind(this), this._internalReject.bind(this));
     }
+
+$Promise.prototype._callHandlers = function () {
+    while(this._handlerGroups.length){
+        const group = this._handlerGroups.shift();
+        if (this._state === 'fulfilled' && group.successCb)group.successCb(this._value);
+        if (this._state === 'rejected' && group.errorCb)group.errorCb(this._value);
+    }
+};
 
 $Promise.prototype._internalResolve= function (data) {
     if(this._state=== 'pending'){
     this._state = 'fulfilled';
     this._value = data;
+    this._callHandlers();
     }
 };
 $Promise.prototype._internalReject= function (reason){
     if(this._state=== 'pending'){
     this._state = 'rejected';
     this._value = reason;
+    this._callHandlers();
     }
 };
+
+$Promise.prototype.then = function (successCb, errorCb) {
+    this._handlerGroups.push({
+        successCb: typeof successCb === "function" ? successCb : false,
+        errorCb: typeof errorCb === "function" ? errorCb : false,
+    });
+    if (this._state === 'fulfilled' || this._state === 'rejected') this._callHandlers();
+};
+
+$Promise.prototype.catch = function (errorCb) {
+    return this.then(null, errorCb);
+};
+
 
 module.exports = $Promise;
 /*-------------------------------------------------------
