25fbfb877d21f2ea373377f40fec0856bbe3176d
Johan Sebastian Castro Lopez <sebastiancastro0792@gmail.com>
2022-12-28 14:16:56 -0500

    se agrega M4, y se realiza testing

--- FT-M3-master/06-Testing/homework/index.js
+++ FT-M3-master/06-Testing/homework/index.js
@@ -1,17 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    message: 'hola',
+    message: "hola",
   });
 });
 
-app.post('/product', (req, res) => {
+app.get("/test", (req, res) => {
   res.send({
-    result: req.body.a / req.body.b,
+    message: "test",
+  });
+});
+
+app.post("/sum", (req, res) => {
+  res.send({
+    result: 5,
+  });
+});
+
+app.post("/product", (req, res) => {
+  res.send({
+    result: req.body.a * req.body.b,
+  });
+});
+
+function sumArray(array, targetNum) {
+  let left = 0,
+    right = array.length - 1;
+  while (left < right) {
+    const sum = array[left] + array[right];
+    if (sum === targetNum) return true;
+    sum < targetNum ? left++ : right--;
+  }
+  return false;
+}
+
+app.post("/summArray", (req, res) => {
+  const { array, num } = req.body;
+
+  res.send({
+    result: sumArray(array, num),
   });
 });
 
